baf700a551aa7114e3cf7111dce37799c24ea2cb
Flo <teorta3@gmail.com>
2025-10-13 13:59:03 +0200

    Premier

--- test.c
+++ test.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int secondes;
+    printf("Entrez un nombre de secondes : ");
+    scanf("%d", &secondes);
+    int heures = secondes / 3600;
+    int minutes = (secondes % 3600) / 60;
+    int sec = secondes % 60;
+    printf("%d secondes = %d heures, %d minutes et %d secondes\n", secondes, heures, minutes, sec);
 }
