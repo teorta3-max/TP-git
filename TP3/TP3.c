5adbc796aea405600139fb4888eda60bdb21f52a
Flo <teorta3@gmail.com>
2025-11-17 13:25:45 +0100

    TP3 étape 1 vrai

--- TP3/TP3.c
+++ TP3/TP3.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-// Exercice 1
-int main(){
+int main() {
     int taille;
-    printf(" saisir une taille n (5 <= n >= 10):");
-    scanf("%d",&taille);
-    if (taille > 10 || taille < 5){
-        printf("La taille doit etre superieur ou égale à 5 et inférieur ou égal à 10");
+
+    printf("Saisir une taille n (5 <= n <= 10) : ");
+    scanf("%d", &taille);
+    while (taille < 5 || taille > 10) {
+        printf("Erreur : la taille doit etre comprise entre 5 et 10.\n");
+        printf("Saisir une taille n (5 <= n <= 10) : ");
+        scanf("%d", &taille);
     }
     int width = taille * taille;
     int max_height = 3 * taille - 1;
     char img[100][100];
-
+    
     
 }
