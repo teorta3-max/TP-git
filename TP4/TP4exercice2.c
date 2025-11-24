229ccdb9b969641c98bd95d978e07ad70ae6f0e6
Flo <teorta3@gmail.com>
2025-11-24 15:31:38 +0100

    Etape 1

---
+++ TP4/TP4exercice2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Etape 1 : Afficher le menu 
+
+void afficher_menu() {
+    printf("=== BANQUE - DISTRIBUTEUR ===\n");
+    printf("1. -Faire un retrait\n");
+    printf("2. -Quitter\n");
+}
+
+int main() {
+    afficher_menu();
+}
