0e1c57120061104ec9af0e17b0bfe585913f6d98
Flo <teorta3@gmail.com>
2025-11-24 12:55:45 +0100

    Etape 1

---
+++ TP4/TP4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//Étape 1 : Fonction afficherMenu
+
+void afficherMenu() {
+    printf("===== GESTION DES NOTES =====\n");
+    printf("1. Saisir le nombre d'eleves\n");
+    printf("2. Saisir les notes des eleves\n");
+    printf("3. Afficher toutes les notes\n");
+    printf("4. Afficher la moyenne d'un eleve\n");
+    printf("5. Afficher la moyenne generale\n");
+    printf("6. Afficher la meilleure note de chaque controle\n");
+    printf("0. Quitter\n");
+}
+
+int main() {
+    afficherMenu();
+    return 0;
+}
