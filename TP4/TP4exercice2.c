8e109fc80de5904a14f5fa3b44c7a6b4ccc4ffb7
Flo <teorta3@gmail.com>
2025-11-24 15:37:46 +0100

    Etape 2

--- TP4/TP4exercice2.c
+++ TP4/TP4exercice2.c
@@ -8,6 +8,24 @@ void afficher_menu() {
     printf("2. -Quitter\n");
 }
 
+//Etape 2 : Demander un montant 
+
+int saisir_montant() {
+    int montant;
+    printf("Montant a retirer: ");
+    scanf("%d", &montant);
+    return montant;
+}
+
+
 int main() {
     afficher_menu();
+    int choix;
+    scanf("%d", &choix);
+    if (choix == 1) {
+        int x = saisir_montant();
+    } 
+    else {
+        
+    }
 }
