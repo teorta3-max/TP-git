329b2f8e5a1fc7aac79238e88d8a6bacd22d2478
Flo <teorta3@gmail.com>
2025-11-24 15:55:27 +0100

    Final

--- TP4/TP4exercice2.c
+++ TP4/TP4exercice2.c
@@ -39,19 +39,40 @@ void calcul_distribution(int montant) {
 
     printf("Distribution des billets :\n");
     if (billets_50 > 0){
-        printf("Billets de 50€ : %d\n", billets_50);
+        printf("Billets de 50 euro : %d\n", billets_50);
     }
     if (billets_20 > 0){
-        printf("Billets de 20€ : %d\n", billets_20);
+        printf("Billets de 20 euro : %d\n", billets_20);
     } 
     if (billets_10 > 0){
-        printf("Billets de 10€ : %d\n", billets_10);
+        printf("Billets de 10 euro : %d\n", billets_10);
     }
     if (billets_5 > 0){
-       printf("Billets de 5€ : %d\n", billets_5); 
+       printf("Billets de 5 euro : %d\n", billets_5); 
     } 
 }
 
+
 int main() {
-    //On verra quand on aurra toutes les fonctions
+    afficher_menu();
+    int choix;
+    while (choix != 2) {
+        printf("Choisissez une option: ");
+        scanf("%d", &choix);
+        if (choix == 1) {
+            int montant = saisir_montant();
+            if (montant_valide(montant)) {
+                calcul_distribution(montant);
+            } else {
+                printf("Montant invalide. Veuillez reessayer.\n");
+            }
+        } else if (choix == 2) {
+            printf("Merci d'avoir utilise le distributeur. Au revoir!\n");
+        } else {
+            printf("Option invalide. Veuillez reessayer.\n");
+        }
+        afficher_menu();
+    }
+    
+    
 }
