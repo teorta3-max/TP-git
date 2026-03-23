61a54220abb1061bffb94c9bcd3076395f120a43
Flo <teorta3@gmail.com>
2026-03-23 14:03:53 +0100

    Test de la fonction et elle marche

--- TP4_TU_test/TP4_TU_test.cpp
+++ TP4_TU_test/TP4_TU_test.cpp
@@ -12,10 +12,26 @@ namespace TP4TUtest
 	TEST_CLASS(TP4TUtest)
 	{
 	public:
-		
-		TEST_METHOD(TestESTpair)
+		TEST_METHOD(TestESTpair1)
 		{
-			Assert::AreEqual(1, estPair(6));
+			Assert::AreEqual(estPair(4) , 1);
 		}
+		TEST_METHOD(TestESTpair2)
+		{
+			Assert::AreEqual(estPair(7), 0);
+		}
+		TEST_METHOD(TestESTpair3)
+		{
+			Assert::AreEqual(estPair(0), 1);
+		}
+		TEST_METHOD(TestESTpair4)
+		{
+			Assert::AreEqual(estPair(-2), 1);
+		}
+		TEST_METHOD(TestESTpair5)
+		{
+			Assert::AreEqual(estPair(-3), 0);
+		}
+		
 	};
 }
