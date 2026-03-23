da3345874736e00f4bcdee7a96c83b4877fcf6a2
Flo <teorta3@gmail.com>
2026-03-23 15:11:38 +0100

    Corrigée

--- TP4_TU/TP4_TU.c
+++ TP4_TU/TP4_TU.c
@@ -43,7 +43,7 @@ int divisionExacte(int a, int b) {
 
 int sommeTableau(const int tab[], int taille) {
     int somme = 0;
-    for (int i = 0; i <= taille; i++) {
+    for (int i = 0; i < taille; i++) {
         somme += tab[i];
     }
     return somme;
